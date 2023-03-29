54bb3b988d2a4a125db8b86614a3e0fce9574f0d
Nelson <exio78@hotmail.com>
2023-03-29 11:26:13 -0300

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,11 +2,12 @@ import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { SyntheticEvent, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
 
 export default observer(function ActivityList() {
   
   const { activityStore } = useStore();
-  const { activitiesByDate, selectActivity, deleteActivity, loading } = activityStore;
+  const { activitiesByDate, deleteActivity, loading } = activityStore;
 
   const [target, setTarget] = useState('');
 
@@ -28,7 +29,7 @@ export default observer(function ActivityList() {
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
                 <Button 
                   name={activity.id}
                   loading={loading && target === activity.id} 
